c793e77e112e3bc74410f8feb2fa4f66d336f838
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-06-22 10:11:42 -0300

    Iniciando o SignUp

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,34 @@
+import styled from 'styled-components';
 
 export default function SignUp(){
     return(
-        <div>Sign Up Page</div>
+        <>
+            <Logo>
+                MyWallet
+            </Logo>
+            <InputFields>
+                <input placeholder='Nome' />
+                <input placeholder='Email' />
+                <input placeholder='Senha' />
+                <input placeholder='Confirme a senha' />
+            </InputFields>
+        </>
+
     )
 }
 
+
+const Logo =  styled.div`
+    font-family: 'Saira Stencil One', cursive;
+    font-size: 32px;
+    color: #FFF;
+    text-align: center;
+    margin-top: 160px;
+`;
+
+const InputFields = styled.div`
+    width: 80%;
+    margin: 26px auto 0 auto;
+
+`;
 
